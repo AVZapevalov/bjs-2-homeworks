fa85c6f4fa5e693eb05b4ae032b11ee4bcfc1ba4
alexandrzapevalov <avzapevalov@gmail.com>
2023-01-10 18:57:31 +0000

    HomeWork4_Object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
+  }
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (marksToAdd.length !== 0 && this.hasOwnProperty('marks')) {
+      this.marks.push(...marksToAdd);
+    }
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+    if (!this.hasOwnProperty('marks') ||  this.marks.length === 0) {
+      return 0;
+    }
+    return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
